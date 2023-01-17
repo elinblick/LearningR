2082c39c352867d1d35accc297a5cf9c787eb467
Elin Blick <elinbli@chalmers.se>
2023-01-17 11:17:19 +0100

    Making enhance small data set.

--- R/learning.R
+++ R/learning.R
@@ -23,6 +23,37 @@ summary(airquality)
 # Packages ----------------------------------------------------------------
 
 library(tidyverse)
+library(NHANES)
 
 # This will be used for testing out Git
+
+
+# Looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+colnames(NHANES)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+nhanes_small <- select(
+    NHANES,
+    Age,
+    Gender,
+    BMI,
+    Diabetes,
+    PhysActive,
+    BPSysAve,
+    BPDiaAve,
+    Education
+)
+
+nhanes_small
 
